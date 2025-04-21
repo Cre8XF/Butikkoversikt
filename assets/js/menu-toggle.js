99177e437234613f4b39e42b5171d89504c1b4e0
DNTredbull <rogsor80@gmail.com>
2025-04-21 06:54:25 +0200

    new mwnu

---
+++ assets/js/menu-toggle.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const toggle = document.getElementById('menuToggle');
+  const menu = document.getElementById('sideMenu');
+  const overlay = document.getElementById('overlay');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+
+  overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+  });
+});
