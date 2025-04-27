4ff573bd53b184377855354f4d9bef01f82f3285
DNTredbull <rogsor80@gmail.com>
2025-04-27 15:12:33 +0200

    z

--- assets/js/generate/generate-alle-butikker.js
+++ assets/js/generate/generate-alle-butikker.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let butikker = [];
 
-  fetch("assets/butikker.json")
+  fetch("assets/data/butikker.json")
     .then(response => response.json())
     .then(data => {
       butikker = data;
