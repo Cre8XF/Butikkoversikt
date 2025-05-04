14f1b6407ee3c0ac5cc94fe84e0787b4962ae282
DNTredbull <rogsor80@gmail.com>
2025-05-05 00:45:33 +0200

    ggg

--- assets/js/generate/generate-alle-butikker-filter-tags-enhanced.js
+++ assets/js/generate/generate-alle-butikker-filter-tags-enhanced.js
@@ -168,7 +168,7 @@ function visButikker(butikker) {
     col.className = "col-12 col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch";
 
     const card = document.createElement("div");
-    card.className = "store-card text-center w-100";
+    card.className = "store-card top25-card text-center w-100";
 
     card.innerHTML = `
       <a href="${butikk.url}" target="_blank" rel="noopener" class="text-decoration-none text-dark">
